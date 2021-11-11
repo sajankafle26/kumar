3bb9e4f05b73608563653d878ac642d5fd77744f
sajankafle26 <64882150+sajankafle26@users.noreply.github.com>
2021-11-11 08:50:21 +0545

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.seven').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:true
+            },
+            1000:{
+                items:7,
+                nav:true,
+                loop:true
+            }
+        }
+    })
+  });
